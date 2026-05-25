2fb3d4a9adfc2eb30ece8c92fdbfafada437c9e3
Blame_George <gkupatadze750@gmail.com>
2026-05-25 20:19:35 +0400

    Add new blog posting functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if (req.session.user) {
+    return res.redirect('/blogs');
+  }
+  res.redirect('/login');
 });
 
 module.exports = router;
