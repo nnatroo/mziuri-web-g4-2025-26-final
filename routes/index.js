18c787fad0728d814246a1d0a0229b4c76a7dc24
Blame_George <gkupatadze750@gmail.com>
2026-05-25 20:21:37 +0400

    Change var into const

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 router.get('/', function(req, res, next) {
   if (req.session.user) {
     return res.redirect('/blogs');
   }
+
   res.redirect('/login');
 });
 
