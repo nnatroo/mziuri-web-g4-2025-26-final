acb9027575e4fa64ddf2bdd17ba33e6b08bdb4ca
nnatroo <nugonatroshvili@gmail.com>
2026-06-03 20:14:49 +0400

    Add logout functionality with session destruction

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const indexRouter = require('./routes/index');
 const registerRouter = require('./routes/register');
 const loginRouter = require('./routes/login');
 const blogsRouter = require('./routes/blogs');
+const logoutRouter = require('./routes/logout');
 
 const app = express();
 connectDB();
@@ -38,6 +39,7 @@ app.use('/', indexRouter);
 app.use('/register', registerRouter);
 app.use('/login', loginRouter);
 app.use('/blogs', blogsRouter);
+app.use('/logout', logoutRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
