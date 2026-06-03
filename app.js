a491cb6668f937b11f49fcb38987bc59492edbe0
nnatroo <nugonatroshvili@gmail.com>
2026-06-03 19:39:33 +0400

    Add environment configuration for MongoDB connection

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const connectDB = require('./db');
 const session = require("express-session")
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const registerRouter = require('./routes/register');
